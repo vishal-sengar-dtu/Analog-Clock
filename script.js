7c5f83e138067d37a580bd22ab8f09e2a6194026
vishal-sengar-dtu <68491371+vishal-sengar-dtu@users.noreply.github.com>
2020-10-02 21:46:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hr = document.querySelector("#hr");
 const mn = document.querySelector("#mn");
 const sc = document.querySelector("#sc");
 
-setInterval(() => {
+const clock = () => {
   let day = new Date();
   let hh = day.getHours() * 30;
   let mm = day.getMinutes() * deg;
@@ -12,4 +12,7 @@ setInterval(() => {
   hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
-}, 1000);
+};
+
+clock();
+setInterval(clock, 1000);
